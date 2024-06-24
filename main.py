b730f176fbbaea780b3e3211abba26dcb730374c
stanislavets <stanislavets2009@mail.ru>
2024-06-24 16:48:52 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+a=("молодежь")
+print(len(a))
+first=22
+second=15
+summa= first+second
+diff=first-second
+print(summa)
+print(diff)
+first=12
+second=17
+third=13
+numbers = (first, second, third)
+mean = (sum(numbers) / len(numbers))
+print(mean)
+first_string="вторник"
+second_string="понедельник"
+print(second_string + (", ") + first_string)
+a=2
+b=3
+c=4
+f=(a * b) + (a * c)
+print((f**3)/2)
